93b5a646d432bb2ae9fb9e009e4cef4fd84e8cc5
Adam Foosaner <nucat176@gmail.com>
2019-02-13 18:28:50 -0800

    adds comments to user account model to clarify some of the attributes

--- server/models/useraccount.js
+++ server/models/useraccount.js
@@ -1,44 +1,47 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const UserAccount = sequelize.define('UserAccount', {
-      apiId: {
-          allowNull: false,
-          unique: true,
-          type: DataTypes.INTEGER
-      },
-      apiKey: {
-          allowNull: false,
-          unique: true,
-          type: DataTypes.STRING
-      },
-      secretKey: {
-          allowNull: false,
-          unique: true,
-          type: DataTypes.STRING
-      },
-      publicKey: {
-          allowNull: false,
-          unique: true,
-          type: DataTypes.STRING
-      },
-      privateKey: {
-          allowNull: false,
-          unique: true,
-          type: DataTypes.STRING
-      },
-      otpKey: {
-          type: DataTypes.STRING
-      },
-      otpExp: {
-          type: DataTypes.BIGINT
-      },
-      active: {
-          type: DataTypes.BOOLEAN,
-          allowNull: false
-      }
-  }, {});
-  UserAccount.associate = function(models) {
-    // associations can be defined here
-  };
-  return UserAccount;
+    const UserAccount = sequelize.define('UserAccount', {
+        //this is the id as defined in the API itself, not the MW
+        apiId: {
+            allowNull: false,
+            unique: true,
+            type: DataTypes.INTEGER
+        },
+        apiKey: {
+            allowNull: false,
+            unique: true,
+            type: DataTypes.STRING
+        },
+        secretKey: {
+            allowNull: false,
+            unique: true,
+            type: DataTypes.STRING
+        },
+        publicKey: {
+            allowNull: false,
+            unique: true,
+            type: DataTypes.STRING
+        },
+        privateKey: {
+            allowNull: false,
+            unique: true,
+            type: DataTypes.STRING
+        },
+        //stores the one-time passcode for user login
+        otpKey: {
+            type: DataTypes.STRING
+        },
+        //stores the expiration of the one-time passcode
+        otpExp: {
+            type: DataTypes.BIGINT
+        },
+        active: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        }
+    }, {});
+    UserAccount.associate = function (models) {
+        // associations can be defined here
+    };
+    return UserAccount;
 };
