fa0ae5a3756a9967796e8f14ff1e11a91a188ecb
Adam Foosaner <nucat176@gmail.com>
2019-02-20 14:09:00 -0800

    Adds challenge creation function and integration test

--- server/controllers/challenges.js
+++ server/controllers/challenges.js
@@ -1,10 +1,28 @@
 "use strict";
 
 const apiEndpoint = "https://faw5rz7094.execute-api.us-west-1.amazonaws.com/development";
+const UserAccount = require('../models').UserAccount;
+const axios = require("axios");
+const axiosRetry = require("axios-retry");
+const authHelper = require("../helpers/authHelper.js");
+axiosRetry(axios, {retries: 3, retryDelay: axiosRetry.exponentialDelay});
 
 module.exports = {
     async createChallenge (req, res) {
+        const challengeNamespace = req.body.challengeNamespace;
+        const caller = await UserAccount.findOne({where: {apiId: req.session.user.id}});
+        if (!caller) {
+            return res.status(404).send({error: "API Caller account not found"});
+        }
 
+        const createChallengeResp = await axios.post(`${apiEndpoint}/challenge?userId=${caller.apiId}`, {
+            headers: {'Authorization': authHelper.getAuthString(caller.apiKey, caller.secretKey)},
+            data: {
+                challengeNamespace
+            }
+        });
+
+        return res.status(createChallengeResp.status).send(createChallengeResp.data);
     },
 
     async findOneChallenge (req, res) {
--- test/test.js
+++ test/test.js
@@ -2,3 +2,4 @@ const userCreationTest = require('./integration/UserAccount/UserCreationTest.js'
 const sendOtpTest = require('./integration/UserAccount/SendOtpTest.js');
 const userLoginTest = require('./integration/UserAccount/UserLoginTest.js');
 const findOneUserTest = require('./integration/UserAccount/FindOneUserTest.js');
+const createChallengeTest = require('./integration/Challenge/CreateChallengeTest.js');
