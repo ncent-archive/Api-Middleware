9d2ca1f3019ee0490fb6f35791fc639dab714504
Adam Foosaner <nucat176@gmail.com>
2019-03-18 09:21:47 -0700

    fixes routing issue with balances

--- server/routes/index.js
+++ server/routes/index.js
@@ -50,7 +50,7 @@ module.exports = (app) => {
     app.get("/api/challenges", challengesController.findAllChallenges);
 
     //finds all user account balances for a single given challenge
-    app.get("/api/challenges/balances", challengesController.findAllBalancesForChallenge);
+    app.get("/api/challenges/balances/:challengeId", challengesController.findAllBalancesForChallenge);
 
     //shares a challenge from one user to another
     app.patch("/api/challenges/share", challengesController.shareChallenge);
