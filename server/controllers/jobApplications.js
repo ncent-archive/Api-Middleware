b53d80b663cb2b82986234086a80a33525413700
Arya Soltanieh <soltanieharya+github@gmail.com>
2019-04-19 12:47:16 -0700

    Fix error handling for slack

--- server/controllers/jobApplications.js
+++ server/controllers/jobApplications.js
@@ -11,38 +11,44 @@ module.exports = {
     try {
         console.log("creating job application");
         let newJobApplication = await JobApplication.create(req.body);
+        console.log("\njobApplicationsController, new db jobApplication", newJobApplication.dataValues);
+
+        let slackMessage = "";
+        slackMessage += "Role: " + newJobApplication.dataValues.title + "\n";
+        slackMessage += "First Name: " + newJobApplication.dataValues.firstName + "\n";
+        slackMessage += "Last Name: " + newJobApplication.dataValues.lastName + "\n";
+        slackMessage += "Email: " + newJobApplication.dataValues.email + "\n";
+        slackMessage += "Resume: " + newJobApplication.dataValues.resumeURL + "\n";
+        if(newJobApplication.dataValues.phone) {
+            slackMessage += "Phone Number: " + newJobApplication.dataValues.phone + "\n";
+        }
+        if(newJobApplication.dataValues.coverLetterURL) {
+            slackMessage += "Cover Letter: " + newJobApplication.dataValues.coverLetterURL + "\n";
+        }
+        if(newJobApplication.dataValues.githubURL) {
+            slackMessage += "Github: " + newJobApplication.dataValues.githubURL + "\n";
+        }
+        if(newJobApplication.dataValues.githubURL) {
+            slackMessage += "Linked In: " + newJobApplication.dataValues.linkedinURL + "\n";
+        }
+        if(newJobApplication.dataValues.referralCode) {
+            slackMessage += "Refferal Code: " + newJobApplication.dataValues.referralCode + "\n";
+        }
+
+        try {
+            const slackResponse = await web.chat.postMessage({ channel: conversationId, text: slackMessage });
+            console.log('Message sent: ', slackResponse.ts);
+        } catch(error) {
+            console.log("There was a slack error: ", error);
+        }
+        
+
+        // `res` contains information about the posted message
+        
+        return res.status(200).send(newJobApplication);
     } catch(e) {
         console.log("failed to create job application: ", e);
-        return res.status(403).send({ invalidJobApplication: true, errors: e.errors });
+        return res.status(500).send({ invalidJobApplication: true, errors: e.errors });
     }
-    console.log("\njobApplicationsController, new db jobApplication", newJobApplication.dataValues);
-
-    let slackMessage = "";
-    slackMessage += "Role: " + newJobApplication.dataValues.title + "\n";
-    slackMessage += "First Name: " + newJobApplication.dataValues.firstName + "\n";
-    slackMessage += "Last Name: " + newJobApplication.dataValues.lastName + "\n";
-    slackMessage += "Email: " + newJobApplication.dataValues.email + "\n";
-    slackMessage += "Resume: " + newJobApplication.dataValues.resumeURL + "\n";
-    if(newJobApplication.dataValues.phone) {
-    	slackMessage += "Phone Number: " + newJobApplication.dataValues.phone + "\n";
-    }
-    if(newJobApplication.dataValues.coverLetterURL) {
-    	slackMessage += "Cover Letter: " + newJobApplication.dataValues.coverLetterURL + "\n";
-    }
-    if(newJobApplication.dataValues.githubURL) {
-    	slackMessage += "Github: " + newJobApplication.dataValues.githubURL + "\n";
-    }
-    if(newJobApplication.dataValues.githubURL) {
-    	slackMessage += "Linked In: " + newJobApplication.dataValues.linkedinURL + "\n";
-    }
-    if(newJobApplication.dataValues.referralCode) {
-    	slackMessage += "Refferal Code: " + newJobApplication.dataValues.referralCode + "\n";
-    }
-
-    const slackResponse = await web.chat.postMessage({ channel: conversationId, text: slackMessage });
-
-  	// `res` contains information about the posted message
-  	console.log('Message sent: ', slackResponse.ts);
-    res.status(200).send(newJobApplication);
   }
 }
